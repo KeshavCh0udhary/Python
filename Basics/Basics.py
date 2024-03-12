d43891afa69ef6af63cbfc9998d3c0082853736c
Krishna Keshav <Krishna2000chy@gmail.com>
2024-03-12 22:54:20 +0530

    BasicsAdded

---
+++ Basics/Basics.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the number"))
+b = int(input("Enter the number"))
+
+a = a+b
+b = a-b
+a = a-b
+
+print(a,b)
---
+++ Basics/logical.py
@@ -0,0 +1,19 @@
+# write a program to read no of customers
+
+# no_of_tickets = int(input('No of tickets:  '))
+# price_of_ticket = int(input('Price:'))
+
+# print(no_of_tickets*price_of_ticket)
+
+a = int(input('Enter a number'))
+b = int(input('Enter second number'))
+
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a+b)
+print(a-b)
+print(a%b)
+
+
